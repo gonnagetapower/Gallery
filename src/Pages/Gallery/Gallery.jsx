45631a894ab666b0f434da60573274aa59d07b96
gonnagetapower <karpusha201@mail.ru>
2022-05-01 20:22:58 +0300

    adding_TwoFetchFunc

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -26,7 +26,7 @@ const collection = axios.create({
 const Collections = () => {
     const [modalActive, setModalActive] = useState(false)
     const [photo, setPhoto] = useState([])
-    const [page, updatePage] = useState(1)
+    const [page, setPage] = useState(1)
 
     const [modalData, setModalData] = useState({});
 
@@ -37,55 +37,42 @@ const Collections = () => {
 
     const [error, setError] = useState(null)
 
-    useEffect(() => {
-        collection.get(`/photos?page=${page}
-        &page=${page + 1}
-        &page=${page + 2}
-        &page=${page + 3}
-        &page=${page + 4}`).
+    function initialFetch() {
+        collection.get(`/photos?page=${page}`).
             then((response) => {
-                setPhoto(response.data)
-                updatePage(page + 4)
+                setPhoto([...photo, ...response.data])
+                setPage(page + 1)
                 console.log(response.data)
             }).catch(error => {
                 setError(error)
             })
-    }, [])
-
-    // useEffect(() => {
-    //     collection.get(`/search/photos?page=${page}&query=${searchInput}`).
-    //         then((response) => {
-    //             setSearchPhoto(response.data.results)
-    //             console.log(response.data)
-    //         }).catch(error => {
-    //             setError(error)
-    //         })
-    // }, [searchInput])
-
-    async function fetchMorePhoto() {
-        setLoading(true)
-        const response = await collection.get(`/photos?page=${page + 5}`)
-        // updatePage(page + 1)
-        // setPhoto(photo.concat(response.data))
-            updatePage(page + 1)
-            setPhoto(photo.concat(response.data))
-            setLoading(false)
-        // const response1 = await collection.get(`/search/photos?page=${page}&query=${searchInput}`)
-        // if (response1) {
-        //     updatePage(page + 1)
-        //     console.log(response1)
-        //     setSearchPhoto(searchPhoto.concat(response1.data.results))
-        // }
     }
+
+    const searchFetch = async () => {
+        try {
+            const response = await collection.get(`/search/photos?page=${page}&query=${searchInput}`)
+            setSearchPhoto([...searchPhoto, ...response.data.results])
+            setPage(page + 1)
+            console.log(response)
+        }
+        catch (err) {
+            setError(err)
+        }
+    }
+
+    useEffect(() => {
+        initialFetch()
+        searchFetch()
+    }, [searchInput])
 
     if (error) return `Error: ${error.message}`
     return (
         <div class="app__wrapper">
             <Search setSearchInput={setSearchInput} />
-            <Loader active={loading} setLoading={setLoading}/>
+            {/* <Loader active={loading} setLoading={setLoading}/> */}
             <InfiniteScroll
                 dataLength={photo.length}
-                next={fetchMorePhoto}
+                next={!searchInput ? initialFetch : searchFetch}
                 hasMore={true}
                 loader={<h4>Loading...</h4>}
             >
@@ -116,3 +103,48 @@ const Collections = () => {
 }
 
 export default Collections;
+
+
+
+// useEffect(() => {
+//     collection.get(`/photos?page=${page}
+//     &page=${page + 1}
+//     &page=${page + 2}
+//     &page=${page + 3}
+//     &page=${page + 4}`).
+//         then((response) => {
+//             setPhoto(response.data)
+//             updatePage(page + 4)
+//             console.log(response.data)
+//         }).catch(error => {
+//             setError(error)
+//         })
+// }, [])
+
+// useEffect(() => {
+//     collection.get(`/search/photos?page=${page}&query=${searchInput}`).
+//         then((response) => {
+//             setSearchPhoto(response.data.results)
+//             console.log(response.data)
+//             updatePage(page)
+//         }).catch(error => {
+//             setError(error)
+//         })
+// }, [searchInput])
+
+
+
+    // async function fetchMorePhoto() {
+    // setLoading(true)
+    // const response = await collection.get(`/photos?page=${page + 5}`)
+    // updatePage(page + 1)
+    // setPhoto(photo.concat(response.data))
+    // updatePage(page + 1)
+    // setLoading(false)
+    // const response1 = await collection.get(`/search/photos?page=${page}&query=${searchInput}`)
+    // if (response1) {
+    //     updatePage(page + 1)
+    //     console.log(response1)
+    //     setSearchPhoto(searchPhoto.concat(response1.data.results))
+    // }
+    // }
